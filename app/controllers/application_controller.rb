d35efe30f020e5c8c023915856ea94aab7e595ea
Caio <cpenhalver@gmail.com>
2017-02-06 02:24:02 -0200

    Version 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,21 +3,35 @@ class ApplicationController < ActionController::API
   private
 
   def authenticate
-    token = params[:token]
-    return render json: {msg: 'Authentication failed 1'} if token.nil?
-    decoded_token = nil
     begin
-      decoded_token = JWT.decode token, nil, false
-    rescue JWT::ExpiredSignature
-      return render json: {msg: 'Token expired'}
-    rescue JWT::DecodeError
-      return render json: {msg: 'Authentication failed 2'}
-    end
-    md5 = nil
-    decoded_token.each { |i| md5 = i['md5'] if i.has_key?('md5')}
-    @user = User.find_by(token: md5)
-    if @user.nil?
-      render json: {msg: 'Authentication failed 3'}
+      @user = User.find_by(token: md5_hash(token))
+      if @user.nil?
+        render json: {msg: 'Authentication failed'}
+      end
+    rescue TokenError => msg
+      render json: {msg: msg}
     end
   end
+
+  def token
+    token = params[:token]
+    raise TokenError.new('Authentication failed') if token.nil?
+    begin
+      JWT.decode token, nil, false
+    rescue JWT::ExpiredSignature
+      raise TokenError('Token expired')
+    rescue JWT::DecodeError
+      raise TokenError.new('Authentication failed')
+    rescue TokenError
+      raise TokenError.new('Authentication failed')
+    end
+  end
+
+  def md5_hash(decoded_token)
+    decoded_token.each do |i|
+      return i['md5'] if i.has_key?('md5')
+    end
+    raise TokenError('Authentication failed')
+  end
+
 end
---
+++ lib/exceptions/token_error.rb
@@ -0,0 +1,5 @@
+class TokenError < StandardError
+  def initialize(argument = 'Token error')
+    super
+  end
+end
